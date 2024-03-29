605cecf02f88a2e1376a1f17fe59e829ce4678e8
Gabriel Medina <gabi89.m@gmail.com>
2024-03-28 23:43:19 -0300

    Notifications button, desktop version

--- frontend/components/notification-button.tsx
+++ frontend/components/notification-button.tsx
@@ -1,9 +1,36 @@
+'use client'
+
 import { Bell } from "lucide-react";
 import { Button } from "./ui/button";
 import { DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, DrawerDescription, DrawerFooter, DrawerClose, Drawer } from "./ui/drawer";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import Link from "next/link";
+import { useMediaQuery } from "@/hooks/use-media-query"
 
 export default function NotificationButton() {
+
+  const isDesktop = useMediaQuery("(min-width: 768px)")
+
+  if (isDesktop) return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Bell />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="max-w-[360px]">
+        <DropdownMenuLabel>Notifications</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownNotificationItem content="There are new home listings in your area." keepReadingLink="/listings"></DropdownNotificationItem>
+        <DropdownNotificationItem content="Looking for low prices? We&apos;ve got an entire page dedicated to finding you fantastic deals in your favourite places."></DropdownNotificationItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 
   return <>
     <Drawer direction="top">
@@ -16,8 +43,8 @@ export default function NotificationButton() {
           <DrawerDescription>See what&apos;s going on</DrawerDescription>
         </DrawerHeader>
 
-        <NotificationItem content="There are new home listings in your area." keepReadingLink="/listings" />
-        <NotificationItem content="Looking for low prices? We&apos;ve got an entire page dedicated to finding you fantastic deals in your favourite places." />
+        <DrawerNotificationItem content="There are new home listings in your area." keepReadingLink="/listings" />
+        <DrawerNotificationItem content="Looking for low prices? We&apos;ve got an entire page dedicated to finding you fantastic deals in your favourite places." />
 
         <DrawerFooter>
           <DrawerClose asChild>
@@ -34,12 +61,23 @@ type NotificationItemProps = {
   keepReadingLink?: string
 }
 
-function NotificationItem({ content, keepReadingLink }: NotificationItemProps) {
+function DrawerNotificationItem({ content, keepReadingLink }: NotificationItemProps) {
   return <div className="flex items-center text-left gap-4 mt-6 px-4">
     <Bell className="w-[24px] h-[24px] flex-shrink-0" />
     <p className="leading-7">
-      {content}
-      {keepReadingLink && <Link href={keepReadingLink}>Learn more</Link>}.
+      <span>
+        {content}
+        {keepReadingLink && <Link href={keepReadingLink}> Learn more</Link>}.
+      </span>
     </p>
   </div>
+}
+
+function DropdownNotificationItem({ content, keepReadingLink }: NotificationItemProps) {
+  return <DropdownMenuItem>
+    <span>
+      {content}
+      {keepReadingLink && <Link href={keepReadingLink}> Learn more</Link>}.
+    </span>
+  </DropdownMenuItem>
 }
