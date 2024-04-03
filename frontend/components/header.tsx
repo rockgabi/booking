ba596dd20b27f951e751f95533237d73cbb2dc38
Gabriel Medina <gabi89.m@gmail.com>
2024-04-03 14:28:06 -0300

    Partial button styles in header

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -10,8 +10,9 @@ export default async function Header() {
   return (
     <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
       <h1 className="text-2xl font-bold"><Link href="/">Booking</Link></h1>
-      <nav className="flex space-x-4">
+      <nav className="flex space-x-4 items-center">
         <NotificationButton notifications={notifications} />
+        <Link href="/list-your-property" className="hover:bg-opacity-50 hover:bg-destructive hover:text-accent-foreground">List your property</Link>
         {
           session ? (
             <SessionButton />
