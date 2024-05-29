482b18b5615c6f2fabc52743ac9283666d3c2849
nukkerone <gabi89.m@gmail.com>
2024-05-29 15:12:22 -0300

    ssr fix for delayed enter animation

--- frontend/components/ui/delayed-enter-animation.tsx
+++ frontend/components/ui/delayed-enter-animation.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { motion } from "framer-motion"
 
 type Props = {
@@ -11,7 +9,11 @@ type Props = {
 }
 
 export function DelayedEnterAnimation({ children, index, totalItems, stagger = 0.1, duration = 0.5 }: Props) {
-  return <motion.div animate={{
+  return <motion.div initial={{
+    // I want them to start invisible
+    opacity: 0,
+    y: 20,
+  }} animate={{
     // I want them to apply a fade in and a translate up effect
     opacity: [0, 1],
     y: [20, 0],
