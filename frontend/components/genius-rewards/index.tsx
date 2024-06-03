b5579d474d389f3bfff73695647de3f279c18e1f
Gabriel Medina <gabi89.m@gmail.com>
2024-06-02 21:30:25 -0300

    Lucide icons lazy

--- frontend/components/genius-rewards/index.tsx
+++ frontend/components/genius-rewards/index.tsx
@@ -3,19 +3,21 @@
 import geniusRewards from "@/data/genius-rewards";
 import { Carousel, CarouselList } from "../carousel";
 import Link from "next/link";
-import { Car } from "lucide-react";
 import { Card, CardContent } from "../ui/card";
 import { VariantProps, cva } from "class-variance-authority";
 import React from "react";
 import { cn } from "@/lib/utils";
 import { useInView } from "react-intersection-observer";
+import LucideIconLazy from "../ui/lucide-icon-lazy";
+import { DelayedEnterAnimation } from "../ui/delayed-enter-animation";
+import dynamicIconImports from "lucide-react/dynamicIconImports";
 
 export default function GeniusRewards() {
   const { ref, inView, entry } = useInView({
     triggerOnce: true,
     threshold: 0.3,
   });
-  
+
   return <Carousel ref={ref}>
     <CarouselList data={geniusRewards} RenderItem={geniusRewardsItem} carouselItemClassName="basis-1/1 md:basis-1/2 lg:basis-1/3 xl:basis-1/3" shouldAnimate={inView} />
   </Carousel>
@@ -25,31 +27,37 @@ interface GeniusRewardsItem extends React.HTMLAttributes<HTMLAnchorElement>, Var
   heading: string
   subHeading: string
   href: string
+  icon?: keyof typeof dynamicIconImports
 }
 
 const geniusRewardsItemVariants = cva("", {
-    variants: {
-      variant: {
-        primary: "bg-primary text-primary-foreground hover:bg-primary/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-      },
+  variants: {
+    variant: {
+      primary: "bg-primary text-primary-foreground hover:bg-primary/90",
+      outline:
+        "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+      secondary:
+        "bg-secondary text-secondary-foreground hover:bg-secondary/80",
     },
-    defaultVariants: {
-      variant: "primary",
-    },
-  }
+  },
+  defaultVariants: {
+    variant: "primary",
+  },
+}
 )
 
-function geniusRewardsItem({ heading, subHeading, href, variant, className }: GeniusRewardsItem) {
+function geniusRewardsItem({ heading, subHeading, href, variant, icon, className }: GeniusRewardsItem) {
   return <Link href={href} className="flex h-full">
     <Card className={cn("shadow-md", geniusRewardsItemVariants({ variant, className }))}>
       <CardContent className="flex flex-col items-center p-6 gap-6">
         <div className="flex w-full">
           <h3 className="text-lg font-bold">{heading}</h3>
-          <Car className="ml-auto" />
+          {/* This slows a lot the local env server, waiting for fix:  https://github.com/lucide-icons/lucide/pull/2030
+          {icon && <div className="ml-auto">
+            <DelayedEnterAnimation index={0} totalItems={1} shouldAnimate={true} stagger={0.6}>
+              <LucideIconLazy name={icon} />
+            </DelayedEnterAnimation>
+          </div>} */}
         </div>
         <p dangerouslySetInnerHTML={{ __html: subHeading }} />
       </CardContent>
--- frontend/data/genius-rewards/index.tsx
+++ frontend/data/genius-rewards/index.tsx
@@ -1,9 +1,12 @@
+import { LucideIcon } from "lucide-react"
+import dynamicIconImports from "lucide-react/dynamicIconImports"
 
 export type GeniusRewards = {
   heading: string
   subHeading: string
   href: string
   variant: 'primary' | 'outline' | 'secondary' 
+  icon?: keyof typeof dynamicIconImports;
 }
 
 const geniusRewards: GeniusRewards[] = [
@@ -12,24 +15,28 @@ const geniusRewards: GeniusRewards[] = [
     subHeading: `Gabriel, you're at <strong>Genius Level 1</strong> in our loyalty program`,
     href: '/genius-rewards',
     variant: 'primary',
+    icon: 'car'
   },
   {
     heading: '10% discounts on stays',
     subHeading: `Enjoy discounts at participating properties worldwide`,
     href: '/10-stay-discounts',
     variant: 'outline',
+    icon: 'home'
   },
   {
     heading: 'Genius perks',
     subHeading: `Get free breakfast, room upgrades, and more at select properties`,
     href: '/genius-perks',
     variant: 'secondary',
+    icon: 'lamp'
   },
   {
     heading: 'Genius discounts',
     subHeading: `Save on attractions, tours, and more with Genius discounts`,
     href: '/genius-discounts',
     variant: 'secondary',
+    icon: 'dollar-sign'
   }
 ]
 
